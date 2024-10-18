0453a36a59d5d6a39ceeb0807812becabda4fadf
Barrett Ray <66580279+onkoe@users.noreply.github.com>
2024-10-18 17:27:53 -0500

    chore(example): add `tracing_subscriber`

--- libghr/examples/save_as_json.rs
+++ libghr/examples/save_as_json.rs
@@ -1,9 +1,17 @@
 // Let's grab a system `Report` and output it as JSON!
 
 use libghr::report::Report;
+use tracing::Level;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    // turn on logging
+    tracing_subscriber::fmt()
+        .pretty()
+        .with_max_level(Level::DEBUG)
+        .init();
+
+    // make the report
     let report = Report::new().await.unwrap();
     let json_report = serde_json::to_string_pretty(&report).unwrap();
     println!("{}", json_report);
