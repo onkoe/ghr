76ab086fed87188cc5f25f467d8677747a3a6fbc
Barrett Ray <66580279+onkoe@users.noreply.github.com>
2024-10-18 17:28:23 -0500

    refactor(lib/comp/usb): get human-readable

--- libghr/src/report/components/usb.rs
+++ libghr/src/report/components/usb.rs
@@ -1,8 +1,9 @@
 //! i am usb
 
-use super::{ComponentBus, ComponentInfo, ComponentStatus};
 use crate::prelude::internal::*;
 
+use std::path::Path;
+use tokio::join;
 use tokio::try_join;
 
 #[cfg(target_os = "linux")]
@@ -14,16 +15,9 @@ pub async fn usb_components() -> GhrResult<Vec<ComponentInfo>> {
         let path = dev.path();
 
         // read a few files to get important info about this thang
-        let Ok((id, vendor_id, class)) = try_join!(
-            tokio::fs::read_to_string(path.join("idProduct")),
-            tokio::fs::read_to_string(path.join("idVendor")),
-            tokio::fs::read_to_string(path.join("bInterfaceClass")),
-        ) else {
-            tracing::warn!(
-                "A USB component's info was inaccessible! (device at `{})`",
-                path.display()
-            );
-            continue;
+        let ((vendor_id, id), class) = join! {
+            usb_vendor_and_id(&path),
+            usb_class(&path)
         };
 
         usb.push(ComponentInfo {
@@ -37,6 +31,110 @@ pub async fn usb_components() -> GhrResult<Vec<ComponentInfo>> {
     }
 
     Ok(usb)
+}
+
+/// on Linux, this grabs the class codes for our devices.
+#[cfg(target_os = "linux")]
+#[tracing::instrument]
+async fn usb_class(path: &Path) -> String {
+    // read the files
+    let Ok((class, subclass)) = try_join!(
+        tokio::fs::read_to_string(path.join("bDeviceClass")),
+        tokio::fs::read_to_string(path.join("bDeviceSubClass")),
+    ) else {
+        tracing::warn!(
+            "Failed to find device class/subclass! Yielding 'Unknown' for these values."
+        );
+        return "Unknown".into();
+    };
+
+    let (class, subclass) = (class.trim(), subclass.trim());
+
+    // try grabbing a class
+    // look it up
+    if let (Ok(parsed_class), Ok(parsed_subclass)) = (
+        u8::from_str_radix(class, 16),
+        u8::from_str_radix(subclass, 16),
+    ) {
+        if let Some(c) = usb_ids::SubClass::from_cid_scid(parsed_class, parsed_subclass) {
+            return format!("{} ({})", c.class().name(), c.name());
+        }
+    }
+
+    // return the raw ID if we must
+    return format!("{} ({})", class, subclass);
+}
+
+#[cfg(target_os = "linux")]
+#[tracing::instrument]
+async fn usb_vendor_and_id(path: &Path) -> (String, String) {
+    // look for human-readable string repr
+
+    use usb_ids::FromId as _;
+    let human_readable = tokio::join! {
+        tokio::fs::read_to_string(path.join("iManufacturer")),
+        tokio::fs::read_to_string(path.join("iProduct")),
+    };
+
+    // return human-readable strings if they exist for us!
+    if let (Ok(vendor), Ok(product)) = human_readable {
+        tracing::debug!(
+            "oh cool, we got human readable strings! vendor: `{vendor}`. product: {product}."
+        );
+        return (vendor, product);
+    }
+
+    // otherwise, grab lame number
+    let (vend, prod) = tokio::join! {
+        tokio::fs::read_to_string(path.join("idVendor")),
+        tokio::fs::read_to_string(path.join("idProduct")),
+    };
+
+    // trim whitespaces (newlines)
+    let (mut vend, prod) = (
+        vend.map(|s| s.trim().to_string()),
+        prod.map(|s| s.trim().to_string()),
+    );
+
+    // convert them if they exist.
+    if let (Ok(ref num_vend), Ok(ref num_prod)) = (&vend, &prod) {
+        tracing::debug!("lame numbers {}; {}", num_vend, num_prod);
+
+        // try to convert them to human readable names
+        let (parsed_vend, parsed_prod) = (
+            u16::from_str_radix(num_vend, 16),
+            u16::from_str_radix(num_prod, 16),
+        );
+
+        if let (Ok(parsed_vend), Ok(parsed_prod)) = (parsed_vend, parsed_prod) {
+            tracing::debug!("parsed from str radix");
+            if let Some(dev) = usb_ids::Device::from_vid_pid(parsed_vend, parsed_prod) {
+                tracing::debug!("names");
+                return (dev.vendor().name().into(), dev.name().into());
+            }
+
+            // sometimes, the linux usb list is incomplete. let's try just the vendor!
+            if let Some(readable_vend) = usb_ids::Vendor::from_id(parsed_vend) {
+                vend = Ok(readable_vend.name().to_string());
+            }
+        }
+    }
+
+    // otherwise, report errors and return "Unknown" strings
+    if let Err(prod_err) = &prod {
+        tracing::warn!(
+            "Couldn't find product for USB device at path {path:?}. (error: {prod_err})"
+        );
+    }
+    if let Err(vend_err) = &vend {
+        tracing::warn!("Couldn't find vendor for USB device at path {path:?}. (error: {vend_err})");
+    }
+
+    // give up
+    (
+        vend.unwrap_or("Unknown".into()),
+        prod.unwrap_or("Unknown".into()),
+    )
 }
 
 #[cfg(target_os = "windows")]
