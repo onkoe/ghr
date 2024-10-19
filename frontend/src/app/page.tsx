194c24072ee7852e0b80e3c466578bf09bf7a1e8
barrett <onkoe@protonmail.com>
2024-10-19 03:21:40 -0500

    feat(site): add basic navbar + footer
    we'll see if i keep `shadcn/ui`. it's not bad but i dislike `tailwind` and would prefer just writing everything from scratch w/ Material 3

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,27 +2,27 @@ import styles from "./page.module.css";
 import NavBar from "./navbar/page";
 // import Button from "./m3/button/page"
 import ReportListing from "./components/report_listing/page";
+import Footer from "./components/footer/page";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <NavBar />
+    return (
+        <div className={styles.page}>
+            <NavBar />
 
-      <main className={styles.main}>
-        {/* <Button label="hi" onClick={() => console.log("hi clicked")} /> */}
-        <p>{hello_from_actix()}</p>
+            <main className={styles.main}>
+                <h2>GHR - the Global Hardware Report</h2>
+                <p>{hello_from_actix()}</p>
 
-        <ReportListing />
-
-      </main>
-      <footer className={styles.footer}>
-        <p>footer</p>
-      </footer>
-    </div>
-  );
+                <ReportListing />
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 async function hello_from_actix(): Promise<string> {
-  const thing: string = await fetch("http://localhost:8080").then(response => response.text());
-  return thing;
+    const thing: string = await fetch("http://localhost:8080").then(
+        (response) => response.text()
+    );
+    return thing;
 }
