b270ee578e90289547046691d375149e359ec279
barrett <onkoe@protonmail.com>
2024-10-21 22:15:58 -0500

    feat: add `shared` crate for rust-ts interop

--- frontend/src/app/components/report_listing/page.tsx
+++ frontend/src/app/components/report_listing/page.tsx
@@ -1,21 +1,12 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-
-/// A representation of the `libghr::report::Report`. See it for more info. 
-interface Report {
-    os: OsInfo,
-}
-
-interface OsInfo {
-    name: string,
-    version: string,
-    architecture: string,
-}
-
+import ReportPage from "@/app/report/page";
+import type React from "react";
+import { useEffect, useState } from "react";
+import type { WrappedReportTs } from "../../../../../shared/bindings/WrappedReportTs";
 
 const ReportListing: React.FC = () => {
-    const [reports, set_reports] = useState<Report[]>([]);
+    const [reports, set_reports] = useState<WrappedReportTs[]>([]);
     // const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
@@ -25,7 +16,7 @@ const ReportListing: React.FC = () => {
             const json = await resp.json();
             console.log(json);
             set_reports(json);
-        }
+        };
 
         get_reports();
     }, []); // apparently `[]` stops it from doing this... way too much.
@@ -35,13 +26,17 @@ const ReportListing: React.FC = () => {
             <h2>Reports</h2>
             <ul>
                 {reports.map((report) => (
-                    <li key={report.os.name}>
-                        <p>{report.os.name}, {report.os.version} ({report.os.architecture})</p>
+                    <li key={report.id}>
+                        <ReportPage
+                            id={report.id}
+                            recv_time={report.recv_time}
+                            report={report.report}
+                        />
                     </li>
                 ))}
             </ul>
         </div>
-    )
+    );
 };
 
 export default ReportListing;
