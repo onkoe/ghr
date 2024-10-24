e16163148ffbbe50e77d3a4f6d46396b222a0882
Barrett Ray <contact@barretts.club>
2024-10-24 00:10:40 -0500

    feat(lib/rep/gpu/i915): get pci ids for card

--- libghr/src/report/components/gpu/linux/i915.rs
+++ libghr/src/report/components/gpu/linux/i915.rs
@@ -11,6 +11,10 @@ pub(super) async fn gpu(gpu: &Path) -> GhrResult<ComponentInfo> {
         sysfs_value_opt::<String>(gpu.join("device/class")),
     };
 
+    // try converting their names
+    let (id, vendor_id) = convert_to_pci_names(id, vendor_id);
+    let class = convert_to_pci_class(class);
+
     // grab the clock speed, map to mhz
     let clock_speed = sysfs_value_opt::<u64>(gpu.join("gt_max_freq_mhz"))
         .await
