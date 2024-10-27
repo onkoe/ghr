a7f052bf8beb18ccbee5bb787ad3bdf9a744726e
barrett <onkoe@protonmail.com>
2024-10-27 16:09:49 -0500

    feat(lib/rep/storage/linux): uhh. implement it

--- libghr/src/report/components/storage/linux.rs
+++ libghr/src/report/components/storage/linux.rs
@@ -23,9 +23,174 @@ pub(crate) async fn get() -> GhrResult<Vec<ComponentInfo>> {
 /// finds info for one device.
 #[tracing::instrument]
 async fn one<P: AsRef<Path> + Debug>(path: P) -> Option<ComponentInfo> {
-    // (get info)
+    // grab general info
+    let (id, vendor_id) = general_info(&path).await;
 
-    todo!()
+    // and the specialized desc
+    let desc = ComponentDescription::StorageDescription(storage_desc(path).await);
+
+    Some(ComponentInfo {
+        bus: ComponentBus::Unknown, // FIXME: convert the desc's if we got it
+        id,
+        class: None,
+        vendor_id,
+        status: None,
+        desc,
+    })
+}
+
+/// finds general info for the given device.
+///
+/// currently, the return type is: (id, vendor_id)
+#[tracing::instrument]
+async fn general_info<P: AsRef<Path> + Debug>(path: P) -> (Option<String>, Option<String>) {
+    // find the real path
+    let path = path.as_ref();
+
+    tokio::join! {
+        sysfs_value_opt(path.join("device/model")),
+        sysfs_value_opt(path.join("device/vendor"))
+    }
+}
+
+/// finds the specialized device description for the device at `path`.
+#[tracing::instrument]
+async fn storage_desc<P: AsRef<Path> + Debug>(path: P) -> StorageDescription {
+    // grab the real path
+    let path = path.as_ref();
+
+    let (kind, capacity, speed, connector, is_removable) = tokio::join! {
+        kind(path),
+        capacity(path),
+        sysfs_value_opt::<u32>(path.join("queue/rotation_rate")), // rare?
+        connector(path),
+        removable(path),
+    };
+
+    // FIXME: `rotation_rate` isn't well-documented online.
+    //        make sure it exists and isn't unique to like
+    //        four drivers...
+
+    let usage = StorageUsage {
+        usage: None,
+        total_capacity: capacity,
+    };
+
+    StorageDescription {
+        kind,
+        usage,
+        speed,
+        connector,
+        is_removable,
+    }
+}
+
+/// finds the connector for the device at `path`.
+#[tracing::instrument]
+async fn connector<P: AsRef<Path> + Debug>(path: P) -> Option<StorageConnector> {
+    // grab the real path
+    let path = path.as_ref();
+
+    // follow any symlinks
+    let abs = path.canonicalize().unwrap_or(path.to_path_buf());
+
+    // we now have a `<sysroot>/sys/class/<connector>` path.
+    //
+    // let's try to match a `/class/<connector>` from its components.
+    let mut chunks = futures::stream::iter(abs.components()).chunks(2);
+    while let Some(chunk) = chunks.next().await {
+        let first = chunk.first();
+
+        if first.is_none() {
+            break;
+        }
+
+        if let (Some(class), Some(connector)) = (first, chunk.get(1)) {
+            // make sure it's a fr path
+            if class.as_os_str().to_string_lossy() == "class" {
+                // try matching off a connector
+                if let Some(connector) = connector.as_os_str().to_str() {
+                    match connector {
+                        "ata_port" => return Some(StorageConnector::Sata),
+                        "scsi_disk" => return Some(StorageConnector::Scsi),
+                        other => {
+                            tracing::debug!("Failed to map storage connector `{other}`.");
+                        } //
+                          //
+                          // TODO: consider pci? i didn't see it in `/sys/class` though
+                    };
+                }
+            }
+        }
+    }
+
+    None
+}
+
+/// sees if the device at `path` is removable.
+#[tracing::instrument]
+async fn removable<P: AsRef<Path> + Debug>(path: P) -> Option<bool> {
+    // grab the real path
+    let path = path.as_ref();
+
+    // read the `removable` file from `sysfs`
+    let removable_switch = sysfs_value_opt::<u8>(path.join("removable")).await?;
+
+    // map the number `1` to yes, `0` to no... otherwise no clue
+    match removable_switch {
+        1_u8 => Some(true),
+        0_u8 => Some(false),
+
+        other => {
+            tracing::debug!("uhh. this drive was reported to have a removable value of `{other}`!");
+            None
+        }
+    }
+}
+
+/// sees if the device at `path` is rotational.
+#[tracing::instrument]
+async fn kind<P: AsRef<Path> + Debug>(path: P) -> Option<StorageKind> {
+    // grab the real path
+    let path = path.as_ref();
+
+    // read file from the `sysfs`
+    let rotational_switch = sysfs_value_opt::<u8>(path.join("queue/rotational")).await?;
+
+    // map number to bool
+    match rotational_switch {
+        1_u8 => Some(StorageKind::Hdd),
+        0_u8 => Some(StorageKind::Ssd),
+
+        other => {
+            tracing::debug!("uhh... a drive has a rotational value of `{other}`!");
+            None
+        }
+    }
+}
+
+/// attempts to find a device's capacity in KiB.
+#[tracing::instrument]
+async fn capacity<P: AsRef<Path> + Debug>(path: P) -> Option<u64> {
+    let path = path.as_ref();
+
+    // read the value...
+    //
+    // note: it's in sectors!
+    let (sector_count, sector_width) = tokio::join! {
+        sysfs_value_opt::<u64>(path.join("size")),
+        sysfs_value_opt::<u64>(path.join("queue/physical_block_size"))
+    };
+
+    // make sure the values are all good
+    let (Some(sector_count), Some(sector_width)) = (sector_count, sector_width) else {
+        tracing::warn!("Failed to find drive capacity. (sectors: `{sector_count:?}`, width: `{sector_width:?}`)");
+        return None;
+    };
+
+    // B = (sector_size * sector_count)
+    // ...KiB = (B / 1024)
+    Some((sector_width * sector_count) / 1024)
 }
 
 /// finds all (non-partition) entries in the given `<sysfs>/class/block` path.
