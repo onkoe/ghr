e4c3db02b84223759561a754ec58630711e970b0
Barrett Ray <66580279+onkoe@users.noreply.github.com>
2024-10-28 21:03:18 -0500

    chore(lib/rep/nic/windows): add windows stub

--- libghr/src/report/components/nic/windows.rs
+++ libghr/src/report/components/nic/windows.rs
@@ -1,1 +1,8 @@
+use crate::prelude::internal::*;
 
+/// Finds and returns info about network devices on the system.
+#[tracing::instrument]
+pub(crate) async fn get() -> GhrResult<Vec<ComponentInfo>> {
+    tracing::error!("Network interface detection isn't yet implemented on Windows.");
+    Ok(vec![])
+}
