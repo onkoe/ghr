28ba954ae90f97f802eebc1e37ca2fe05880126c
Barrett Ray <66580279+onkoe@users.noreply.github.com>
2024-10-28 23:14:32 -0500

    feat(lib/rep/gpu/linux/generic): get generic IDs

--- libghr/src/report/components/gpu/linux/generic.rs
+++ libghr/src/report/components/gpu/linux/generic.rs
@@ -7,13 +7,18 @@ use crate::prelude::internal::*;
 // (see `asahi-drm` for a nice, readable impl)
 
 #[tracing::instrument]
-pub(super) async fn gpu(_path: &Path) -> GhrResult<ComponentInfo> {
-    // create a useless
+pub(super) async fn gpu(path: &Path) -> GhrResult<ComponentInfo> {
+    // grab class, name, and vendor
+    let (class, id, vendor_id) = {
+        let civ = Civ::new(path).await;
+        (civ.class, civ.id, civ.vendor)
+    };
+
     Ok(ComponentInfo {
         bus: ComponentBus::Pci,
-        id: None,
-        class: None,
-        vendor_id: None,
+        id,
+        class,
+        vendor_id,
         status: None,
         desc: ComponentDescription::None,
     })
