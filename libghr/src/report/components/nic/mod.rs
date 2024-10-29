7318d2b353eccfda0985a86e99ce79eb0bb7c717
Barrett Ray <66580279+onkoe@users.noreply.github.com>
2024-10-28 21:04:28 -0500

    feat(lib/rep): get nics automatically

--- libghr/src/report/components/nic/mod.rs
+++ libghr/src/report/components/nic/mod.rs
@@ -16,3 +16,13 @@ pub struct NicDescription {
     /// The "maximum transfer unit" of a network interface card.
     pub mtu: Option<u32>,
 }
+
+/// Finds and returns info about network devices on the system.
+#[tracing::instrument]
+pub async fn get() -> GhrResult<Vec<ComponentInfo>> {
+    #[cfg(target_os = "windows")]
+    return windows::get().await;
+
+    #[cfg(target_os = "linux")]
+    return linux::get().await;
+}
