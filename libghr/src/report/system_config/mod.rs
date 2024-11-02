1525275785775fc7cf40a6787188adf2b91a9fae
barrett <onkoe@protonmail.com>
2024-11-02 15:29:56 -0500

    feat(lib/cfg): make a `SysConfInfo` constructor

--- libghr/src/report/system_config/mod.rs
+++ libghr/src/report/system_config/mod.rs
@@ -1,13 +1,26 @@
 //! `system_config`: Info about the system's configuration
 
-use sleep::Sleep;
-
-use crate::prelude::internal::*;
-
 pub mod sleep;
 
+use crate::prelude::internal::*;
+use sleep::Sleep;
+
+use futures::FutureExt as _;
+
+/// Information about the system configuration and standard support.
 #[derive(Clone, Debug, PartialEq, PartialOrd, serde::Serialize, serde::Deserialize, TypeScript)]
 #[non_exhaustive]
 pub struct SystemConfInfo {
     pub sleep: Sleep,
+}
+
+impl SystemConfInfo {
+    pub async fn new() -> Self {
+        // we'll get all info using heap-allocated futures.
+        //
+        // this avoids overflowing the stack.
+        let sleep = sleep::get().boxed().await;
+
+        SystemConfInfo { sleep }
+    }
 }
