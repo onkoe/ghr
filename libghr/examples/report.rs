c8756208e10b3cf78053277c35b07e4a9ec198bb
barrett <66580279+onkoe@users.noreply.github.com>
2024-11-02 23:59:42 -0500

    docs(lib): tell ppl how to bench the report gen

--- libghr/examples/report.rs
+++ libghr/examples/report.rs
@@ -1,4 +1,42 @@
-// Just makes a report.
+//! Just makes a report.
+//!
+//! This module uses `tracing-flame` to generate a flamegraph of the library's
+//! execution. There are three steps to get it working:
+//!
+//! 1. `cargo binstall inferno`
+//! 2. `cargo run --example report --release`
+//! 3. `cat tracing.folded | inferno-flamegraph > tracing-flamegraph.svg`
+//!
+//! You may also use `cargo-flamegraph` to get a more detailed view of the
+//! situation. Here are the instructions for that:
+//!
+//! 1. `cargo binstall flamegraph`
+//! 2. Depending on your platform:
+//!     - Windows: `$env:CARGO_PROFILE_RELEASE_DEBUG=true; cargo flamegraph --example report --release`
+//!     - Linux: `CARGO_PROFILE_RELEASE_DEBUG=true cargo flamegraph --example report --release`
+//!
+//! On Windows, you MUST run that command in an elevated command prompt - you'll
+//! get some crazy errors otherwise.
+//!
+//! You may also wish to use `dtrace` on Windows if your debugger isn't working
+//! as you'd expect.
+//!
+//! Here's the instructions for that: https://github.com/microsoft/DTrace-on-Windows
+//!
+//! By the way, if you're not getting debug symbols, make sure Windows Defender
+//! has its defintions updated.
+//!
+//! See: https://github.com/flamegraph-rs/flamegraph/issues/338
+//!
+//! Finally, you might want to add the following to `ghr/Cargo.toml` if you're
+//! having trouble:
+//!
+//! ```toml
+//! [profile.release]
+//! debug = true
+//! strip = false
+//! ```
+
 use libghr::report::Report;
 use tokio::time::Instant;
 use tracing_subscriber::{layer::SubscriberExt as _, util::SubscriberInitExt as _};
